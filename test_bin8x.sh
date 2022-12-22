50aab2db3b4a5bb5f7da7125a3f4a9383768a74c
Thibault Duponchelle <thibault.duponchelle@gmail.com>
2022-12-22 11:28:59 +0100

    Update test_bin8x.sh

--- test_bin8x.sh
+++ test_bin8x.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# This test displays help
+./bin8x -u TEST.83p test.bin -h
+./bin8x -help
+./bin8x -h
+
 # Tests basic
 ./bin8x test.bin
 ./bin8x -i test.bin
 ./bin8x -o test.83p
 ./bin8x -o TEST.83p
 ./bin8x -i test.bin -o TEST.83p
-./bin8x -o TEST.83p test.bin
-./bin8x -o TEST.83p test.bin
-./bin8x -o TEST test.bin
 ./bin8x -i test.bin -o TEST.83p -q -u
 
 # Tests long option
@@ -20,15 +22,6 @@
 ./bin8x -output TEST.83p test.bin  -verbose
 ./bin8x -output TEST.83p test.bin  -executable
 ./bin8x -input test.bin -output TEST.83p -unsquish -unprotected
-./bin8x -output TEST test.bin -name lowercase -lowercase -ti83p
-./bin8x -output TEST test.bin -name lowercase -lowercase -ti83
-./bin8x -output TEST test.bin -name lowercase -lowercase -ti82
-
-
-# This test displays help
-./bin8x -u TEST.83p test.bin -h
-./bin8x -help
-./bin8x -h
 
 # Test autoselect_ext
 ./bin8x test.bin TEST.83p
@@ -39,7 +32,6 @@
 # Test more dangerous
 ./bin8x test.bin TEST.83p -u -q -u
 ./bin8x -q -l -u test.bin TEST.83p
-./bin8x -qe TEST.83p test.bin
 
 # Test some options between input and output
 ./bin8x -q TEST.83p -u test.bin
@@ -57,15 +49,15 @@
 ./bin8x TEST.83p test.bin -q -u
 
 # This one should fail
-#./bin8x -v TEST.83p __temp__.bin -q -u
+./bin8x -v TEST.83p __temp__.bin -q -u
 
 # Compare file generated with explicit -n name
 # Versus name taken from filename
-./bin8x -o TESTTOOLONG.83p test.bin
+./bin8x -o TESTTOOLONG.83p -i test.bin
 ./bin8x -o B.83p -i test.bin -n TESTTOOLONG
 diff TESTTOOLONG.83p B.83p
 
-# Check name \0 padding
+# Check \0 padding (v2.1)
 ./bin8x -o PAD.83p -i test.bin
 ./bin8x -o PADN.83p -i test.bin -n PAD
 diff PADN.83p PAD.83p
@@ -74,4 +66,3 @@ diff PADN.83p PAD.83p
 ./bin2var test.bin PAD.83p
 mv PAD.83p B2V.83p
 ./bin8x -o PAD.83p -i test.bin
-diff PAD.83p B2V.83p
